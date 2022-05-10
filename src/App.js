dd9eea04621545e06b7a1f6f517e546c4d7054c9
jaharvey2000 <jaredh2000@outlook.com>
2022-05-10 16:09:15 -0500

    Added basic text content, not pretty yet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import AppHeader from './components/AppHeader'
+import Home from './pages/Home'
+import About from './pages/About'
+import Skills from './pages/Skills'
+import Experience from './pages/Experience'
+import Contact from './pages/Contact'
+import AppFooter from './components/AppFooter'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AppHeader />
+      <main>
+        <Home />
+        <About />
+        <Skills />
+        <Experience />
+        <Contact />
+      </main>
+      <AppFooter />
+    </>
   );
 }
 
