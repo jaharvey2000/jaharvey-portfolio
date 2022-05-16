c16990f52f5c36b419c5bda6c8b896c9d760f682
jaharvey2000 <jaredh2000@outlook.com>
2022-05-16 17:41:24 -0500

    Refactored skills/experience to use json input

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useNav } from '../customHooks/useNav';
+import { experienceList } from './experienceList';
 
 function ExperienceBlock(props) {
     return (
         <div className="experience">
             <div className="experienceHeader">
                 <h3 className="experienceTitle">{ props.title }</h3>
-                <h3 className="experienceDates">{ props.startDate } - { props.endDate }</h3>
+                <h3 className="experienceDates">{ props.dates }</h3>
             </div>
             <h4>
                 { props.description } <span className="location">@ { props.location }</span>
@@ -28,45 +29,16 @@ function Experience() {
         <section ref={experienceRef}>
             <div id="experienceSection">
                 <h2>Experience</h2>
-                <ExperienceBlock 
-                    title = "Fluid Shift Monitoring"
-                    description = "Senior Capstone Project"
-                    location = "The University of Alabama"
-                    startDate = "August 2021"
-                    endDate = "May 2022"
-                    skillsUsed = "Embedded C, Arduino, Real-Time Operating Systems, MATLAB"
-                    summary = "Lorem ipsum." 
-                />
-
-                <ExperienceBlock 
-                    title = "Methods Time-Management"
-                    description = "Senior Research Project"
-                    location = "The University of Alabama & Mercedes-Benz U.S. International"
-                    startDate = "January 2022"
-                    endDate = "May 2022"
-                    skillsUsed = "C#, .NET, WPF, Kinect Hardware"
-                    summary = "Lorem ipsum." 
-                />
-
-                <ExperienceBlock 
-                    title = "Automated Software Testing Intern"
-                    description = "Internship"
-                    location = "Northrop Grumman, Huntsville, AL"
-                    startDate = "May 2021"
-                    endDate = "July 2021"
-                    skillsUsed = "FrogLogic Squish, Python, Automated Testing, Software Leadership"
-                    summary = "Lorem ipsum." 
-                />
-
-                <ExperienceBlock 
-                    title = "Machine Learning Intern"
-                    description = "Internship"
-                    location = "Northrop Grumman, Huntsville, AL"
-                    startDate = "May 2020"
-                    endDate = "July 2020"
-                    skillsUsed = "Python, Tensorflow, Keras, Scikit-Learn, Pandas, Numpy"
-                    summary = "Lorem ipsum." 
-                />
+                {experienceList.map(({title, description, location, dates, skillsUsed, summary}) => (
+                    <ExperienceBlock 
+                        title={title} 
+                        description={description} 
+                        location={location}
+                        dates={dates} 
+                        skillsUsed={skillsUsed} 
+                        summary={summary} 
+                    />
+                ))}
             </div>
         </section>
     );
--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,5 +1,16 @@
 import React, {useState} from 'react';
 import { useNav } from '../customHooks/useNav';
+import { languages, frontend, backend } from './skillList';
+
+function Skill(props) {
+    return (
+        <div className="skill">
+            <i class={ props.deviconName } />
+            <p class="skillName">{ props.skillName }</p>
+            <p class="skillKnowledge">{ props.level }</p>
+        </div>
+    )
+}
 
 function Skills() {
     // Use Nav Hook
@@ -9,7 +20,7 @@ function Skills() {
         <section ref={skillsRef}>
             <div id="skillsSection">
                 <h2>Skills</h2>
-                <p>
+                {/* <p>
                     Languages: C/C++, Python, Java, JavaScript, C#
                 </p>
                 <p>
@@ -17,7 +28,37 @@ function Skills() {
                 </p>
                 <p>
                     Backend: Node.js, .NET, SQL (Oracle), NoSQL (MondoDB)
-                </p>
+                </p> */}
+                <h3>Languages</h3>
+                <div class="skillList">
+                    {languages.map(({name, level, icon}) => (
+                        <Skill
+                            skillName={name}
+                            level={level}
+                            deviconName={icon}
+                        />
+                    ))}
+                </div>
+                <h3>Frontend</h3>
+                <div class="skillList">
+                    {frontend.map(({name, level, icon}) => (
+                        <Skill
+                            skillName={name}
+                            level={level}
+                            deviconName={icon}
+                        />
+                    ))}
+                </div>
+                <h3>Backend</h3>
+                <div class="skillList">
+                    {backend.map(({name, level, icon}) => (
+                        <Skill
+                            skillName={name}
+                            level={level}
+                            deviconName={icon}
+                        />
+                    ))}
+                </div>
             </div>
         </section>
     );
