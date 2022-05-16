78b410e063814ee1444a439f18faccf8dc2dec65
jaharvey2000 <jaredh2000@outlook.com>
2022-05-16 13:11:49 -0500

    Refactored app sections into Main component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import './App.css';
 import Navbar from './navigation/Navbar'
-import Home from './pages/Home'
-import About from './pages/About'
-import Skills from './pages/Skills'
-import Experience from './pages/Experience'
-import Contact from './pages/Contact'
+import Main from './pages/Main'
 import SiteFooter from './pages/SiteFooter'
 
 function App() {
   return (
     <>
       <Navbar />
-      <main>
-        <Home />
-        <About />
-        <Skills />
-        <Experience />
-        <Contact />
-      </main>
+      <Main />
       <SiteFooter />
     </>
   );
---
+++ src/pages/Main.js
@@ -0,0 +1,19 @@
+import Home from './Home'
+import About from './About'
+import Skills from './Skills'
+import Experience from './Experience'
+import Contact from './Contact'
+
+function Main() {
+    return (
+        <main>
+            <Home />
+            <About />
+            <Skills />
+            <Experience />
+            <Contact />
+        </main>
+    );
+};
+
+export default Main;
